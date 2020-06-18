cbe87d1bd5d46397c73eba662a7e436306e88ed1
majettet4888 <67029936+majettet4888@users.noreply.github.com>
2020-06-18 16:43:37 -0400

    First Submission (Module: 4_ Sales Prediction)

---
+++ P2T1_SalesPrediction_MajetteThelma.py
@@ -0,0 +1,16 @@
+# Get the projected total sales.
+# Date: 14 Jun 2020
+# CTI-110 P2T1 - Sales Prediction
+# Thelma Majette
+#
+
+# Get the projected total sales.
+total_sales = float (input ('Enter the projected sales: '))
+
+#  Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $', format (profit, ',.2f'))
+
+
